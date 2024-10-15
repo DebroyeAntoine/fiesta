2dc92ffb5d3f64fa0b48820cef204cecdd9fa140
Antoine Debroye <ant.debroye@gmail.com>
2024-10-15 21:53:58 +0200

    front: add tailwind dependencies for css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6f61', // exemple couleur vive
+        secondary: '#36d7b7',
+      },
+    },
+  },
+  plugins: [],
+};
+
