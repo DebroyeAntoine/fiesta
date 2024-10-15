ad06c7391c0937b2fd43355ea5a11653ee2e64b1
Antoine Debroye <ant.debroye@gmail.com>
2024-10-15 22:54:31 +0200

    front: Update skull card to be now beautiful !

--- frontend/src/components/GameTable.tsx
+++ frontend/src/components/GameTable.tsx
@@ -29,7 +29,7 @@ const GameTable = () => {
         ))}
       </div>
     </div>
-);
+  );
 };
 
 export default GameTable;
--- frontend/src/components/SkullCard.tsx
+++ frontend/src/components/SkullCard.tsx
@@ -6,17 +6,40 @@ interface SkullCardProps {
   handleValidate: () => void;
 }
 
-const SkullCard: React.FC<SkullCardProps> = ({ word, setWord, handleValidate }) => {
-  return (
-    <div className="bg-primary p-6 rounded-lg shadow-md text-center">
-      <h3 className="text-xl font-bold text-white">Carte Tête de Mort</h3>
-      <input
-        type="text"
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
-        placeholder="Entrez votre indice"
-        className="mt-4 p-2 border border-secondary rounded w-full"
-      />
+const SkullCard: React.FC<SkullCardProps> = ({ word, setWord, handleValidate }) => {return (
+    <div className="relative w-48 h-60 bg-white rounded-xl shadow-md p-4 flex flex-col items-center justify-between border-2 border-gray-800">
+      {/* Crâne */}
+      <div className="w-full flex flex-col items-center relative">
+        {/* Décorations supérieures */}
+        <div className="absolute top-0 w-full flex justify-center">
+          <div className="w-10 h-2 bg-yellow-500 rounded-full mr-2"></div>
+          <div className="w-10 h-2 bg-red-500 rounded-full"></div>
+        </div>
+        {/* Yeux */}
+        <div className="flex justify-between w-full mt-4">
+          <div className="w-10 h-10 bg-black rounded-full border-4 border-orange-500"></div>
+          <div className="w-10 h-10 bg-black rounded-full border-4 border-orange-500"></div>
+        </div>
+        {/* Nez */}
+        <div className="w-4 h-6 bg-black rounded-b-full mt-2"></div>
+        {/* Bouche */}
+        <div className="mt-4 flex space-x-1">
+          <div className="w-3 h-3 bg-black rounded-full"></div>
+          <div className="w-3 h-3 bg-black rounded-full"></div>
+          <div className="w-3 h-3 bg-black rounded-full"></div>
+          <div className="w-3 h-3 bg-black rounded-full"></div>
+        </div>
+      </div>
+      {/* Zone de texte */}
+      <div className="w-full p-2 mt-4 bg-gray-200 rounded-lg flex justify-center items-center">
+        <input
+          type="text"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          className="bg-transparent border-none text-center text-xl font-bold outline-none" 
+          placeholder="Type here" 
+        />
+      </div>
       <button
         onClick={handleValidate}
         className="mt-4 bg-secondary hover:bg-primary text-white font-semibold py-2 px-4 rounded transition duration-300 ease-in-out"
@@ -26,6 +49,5 @@ const SkullCard: React.FC<SkullCardProps> = ({ word, setWord, handleValidate }) 
     </div>
   );
 };
-
 export default SkullCard;
 
