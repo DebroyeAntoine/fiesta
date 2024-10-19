d189f8cbbc7cbde5c4dc180ca019385627ec98db
Antoine Debroye <ant.debroye@gmail.com>
2024-10-19 15:13:17 +0200

    front: Add route to the GameTable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home'; // Page d'accueil
 import RegisterPage from './pages/RegisterPage'; // Page d'inscription
 import LoginPage from './pages/LoginPage'; // Page d'inscription
+import GamePage from './pages/GamePage'; // Page d'inscription
 
 const App = () => {
   return (
@@ -11,7 +12,9 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        {/* Ajoute d'autres routes ici */}
+        <Route path="/game/:gameId/player/:playerId/round/:roundId" element={<GamePage />} />
+
+      {/* Ajoute d'autres routes ici */}
       </Routes>
     </Router>
   );
